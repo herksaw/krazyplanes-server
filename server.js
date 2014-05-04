2f58cee7c15684325ed025542dbad85b2e2e9a01
herksaw <herksaw@gmail.com>
2014-05-04 18:59:47 +0800

    Added some comment.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var mongoose = require('mongoose');
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World');
+	res.end('Krazy Planes Server'); // For testing purpose
 }).listen(8080, '192.237.247.72'); //192.237.247.72
 
+// Connect to our database
 mongoose.connect('mongodb://127.0.0.1:27017/krazyplanes', function(err) {
 	if (!err) {
 		console.log("connected to mongodb");
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/krazyplanes', function(err) {
 	}
 });
 
+// Restricted to object scope
 /*
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -32,7 +34,7 @@ var Player = mongoose.model('Player', PlayerSchema);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
-	console.log("connection")
+	console.log("connection") // Indicated connection is established with client
 	socket.on('player_send', function(data) {
 		console.log("player_send")
 		jsonData = JSON.parse(data);
@@ -42,6 +44,7 @@ io.sockets.on('connection', function(socket) {
 			fbid: jsonData.fbid
 		});
 		
+		// Search for the document which is matched with the field
 		Player.findOne({ 'fbid': player.fbid }, function(err, foundPlayer) {
 			if (foundPlayer !== null) {
 				if (foundPlayer.score < player.score) {
@@ -65,6 +68,7 @@ io.sockets.on('connection', function(socket) {
 	})
 });
 
+// Check if the string a valid json object
 function isJSONString(string) {
 	try {
 		JSON.parse(string);
